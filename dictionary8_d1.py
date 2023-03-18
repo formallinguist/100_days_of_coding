7284dadac96e989a3df971e09a82c4a51bb9280a
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-18 18:23:54 +0100

    Add files via upload

---
+++ dictionary8_d1.py
@@ -0,0 +1,11 @@
+product = {
+    'name' : '',
+    'unit_price' :0,
+    'taxable' :True,
+    'in_stock' :0,
+    'Models': ['Black','Tortoise']
+    }
+
+DWCOO1 = dict.fromkeys(product.keys())
+
+print(DWCOO1)
