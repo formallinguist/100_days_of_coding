75f4a82a2d4a64a81e4caebc381297bdd96edb48
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-18 18:22:37 +0100

    Add files via upload

---
+++ dictionary5_d1.py
@@ -0,0 +1,13 @@
+product = {
+    'name' : 'Ray-Ban Wayfarer Sungalsses',
+    'unit_price' :  112.99,
+    'taxable' : True,
+    'in_stock' : 10,
+    'models': ['Black','Tortoise']
+    }
+
+print(product['name'])
+print(product['unit_price'])
+print(product['taxable'])
+print(product['in_stock'])
+print(product['models'])
