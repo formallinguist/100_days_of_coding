227a5834ae25f7f27bd56db1d549f9f8bf5936fe
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-18 18:20:29 +0100

    Add files via upload

---
+++ dictionary2_d1.py
@@ -0,0 +1,15 @@
+people = {
+    'htanaka':'Haru Tanaka',
+    'zmin'   : 'Zhang Min',
+    'afarooqi' : 'Ayesha Farooqi',
+}
+
+# show original people dictionary.
+#print(people)
+
+print(people)
+
+adios = people.pop('zmin')
+
+print(people)
+print(adios)
