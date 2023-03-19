c9571264e48f4ea63f5aa73c4351d86d8986e1dc
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-19 23:39:17 +0100

    Add files via upload

---
+++ function3_d2.py
@@ -0,0 +1,8 @@
+def hello(fname, lname, datestring):
+    """A docstring describing the function"""
+    msg = "Hello " + fname + " " + lname
+    msg += "you mentioned " + datestring
+    print(msg)
+
+hello("Alan ","Simpson ", "12/31/2019")
+
