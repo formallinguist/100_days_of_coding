34ac9f3a11c28a1d1858d9f4f6c7c726e3fe6bbc
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-23 16:49:02 +0100

    Add files via upload

---
+++ Program_d6.py
@@ -0,0 +1,22 @@
+# Here we are using an inbuilt function which is print.
+print(2+3) # addition
+print(9-8) # substraction
+print(4*6) # multiplication
+print(8/4) # division when we divide we get float.
+print(5/2) # divisin when we want float output.
+print(5//2) #division when we want integer output.
+print(8 + 9 - 10)
+print(8 + 2 * 3)
+print((8 + 2) * 3) #using bodmas rule.
+print(2 * 2 * 2) # for power of or exponents.
+print(2 ** 3 ) # ** is used for exponents.
+print(10 % 3) # It will give us a reminder.
+print('Ravi') # string.
+print("Ravi's laptop") #Double quotes for strings.
+print('Ravi "laptop"') # Single quotes for strings.
+print('Ravi\'s "laptop"') # \ is used to make python ignore the special meaning.
+print(10 * "Ravi")
+print("c:\docs\navin") # \n is used for next line.
+print(r'c:\docs\navin') # r is used for raw string that is print as it is.
+ 
+ 
