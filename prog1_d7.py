cf30c2cdf6351465c83c24210fa195d8be436297
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-24 14:14:41 +0100

    Add files via upload

---
+++ prog1_d7.py
@@ -0,0 +1,50 @@
+print(2+3)
+
+x = 2
+print(x + 3)
+
+y = 3
+print(x + y)
+
+x = 9
+print(x + y)
+
+print(x)
+
+print(x + 10)
+
+z = x + y
+
+print(z)
+
+name = "Youtube"
+
+print(name)
+
+print( name +" " + "Rocks")
+
+print(name[0])
+
+print(name[6])
+
+print(name[-1])
+
+print(name[-2])
+
+print(name[-7])
+
+print(name[0:2])
+
+print(name[1:4])
+
+print(name[1:])
+
+print(name[:4])
+
+print(name[3:10])
+
+print("My" + name[3:])
+
+myname = "RAVI KIRAN CHIKKALA"
+
+print(len(myname))
