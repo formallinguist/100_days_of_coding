b56a74ec38362aeaf325d9c3af105332c5b8a4ce
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-25 15:06:44 +0100

    Add files via upload

---
+++ prog1_d8.py
@@ -0,0 +1,16 @@
+#Tuples
+
+tup = (1,2,3,4,5,6) # we cannot change vales in tuple.
+
+print(tup[1])
+
+print(tup)
+
+
+#Set.
+s = {1,2,3,4,5,6,7,8} # set doesnot take duplicates.
+print(s) #it doesnot follow the order as we have give or entered but it has all values.
+
+s = {1,1,2,2,4,5,6,7,8}
+
+print(s) #set doesnot support indexing as t he elements are not in order.
