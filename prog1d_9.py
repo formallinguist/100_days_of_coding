7250507c278b5ead1526fe218172e79e1c025b70
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-26 23:38:57 +0200

    Add files via upload

---
+++ prog1d_9.py
@@ -0,0 +1,44 @@
+#More on variables.
+num = 5
+
+id (num)
+
+name = "navin"
+
+id (name)
+
+a = 10
+b = a
+
+print(a)
+
+print(b)
+
+print(id(a))
+
+print(id(b))
+
+print(id(10))
+
+k = 10
+
+print(id(k))
+
+a = 9
+
+print(id(a))
+
+print(id(a))
+
+k = a
+
+print(id(k))
+
+#Creating a constant.
+
+PI = 3.14
+
+print(PI) #However we can change the value, generally in python we cannot create constants.
+
+print(type(PI))
+
