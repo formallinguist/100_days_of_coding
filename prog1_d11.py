f48950c2a95fa1e974498c6427b735c9d3c8d95d
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-29 01:29:35 +0200

    Add files via upload

---
+++ prog1_d11.py
@@ -0,0 +1,74 @@
+#Operators in Python.
+x = 2
+print(x)
+
+y = 3
+print(y)
+
+print(x + y)
+
+print(x - y)
+
+print(x * y)
+
+print(x / y)
+
+#Here we are incrementing x with 2 numbers.
+x = x + 2
+
+print(x)
+
+
+#This is another way of incrementing a variable.
+x += 2
+print(x)
+
+x *= 3
+print(x)
+
+a,b = 5,6
+print(a)
+print(b)
+
+n = 7
+print(n)
+
+-n
+print(-n)
+
+#Relational operators.
+
+a = 4
+b = 7
+
+print(a < b)
+
+print( a > b)
+
+print( a == b)
+
+print (a <= b)
+
+print (a >= b)
+
+print (a != b)
+
+#Logical operators.
+
+a = 5
+b = 4
+
+print (a < 8 and b < 5)
+
+print (a < 8 and b < 2)
+
+print (a < 8 or b < 2)
+
+x = True
+
+print ( not x)
+
+print(x)
+
+
+
