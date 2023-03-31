c84c9e700a090327647e885347505e22cfe939d9
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-31 14:19:14 +0200

    Add files via upload

---
+++ prog1_d14.py
@@ -0,0 +1,29 @@
+
+print ("hello.py")
+
+x = 5
+y = 4
+
+print(x + y )
+
+# How to ask for the user for an input.
+
+a = int(input('Enter a value: ')) #Input function gives you the string vlaues.
+b = int(input('Enter a value: '))
+
+print("Your requested output is", a + b)
+
+# we are giving whole input string to char and printing only the first character.
+chr = input('Enter the value')
+print(chr[0])
+
+# We assigning only first character to the char variable not the whole input.
+
+char = input('Enter a value: ') [0]
+print(char)
+
+#Eval function for expressions.
+
+result = eval(input('Enter the expression: '))
+print('The result is', result)
+
