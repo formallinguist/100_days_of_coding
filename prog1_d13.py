5352e2e2fec4614b77ad66706a3a09fc006268d8
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-31 02:21:46 +0200

    Add files via upload

---
+++ prog1_d13.py
@@ -0,0 +1,46 @@
+#Complimet operator.
+
+print(~12)
+
+#Bitwise operator.
+print(12 & 13)
+
+print( 12 | 13)
+
+print(25 & 30)
+
+print(12 ^ 1)
+
+print(12 ^ 13)
+
+print(25 ^ 30)
+
+print(10 << 2)
+
+print( 10 >> 2)
+
+import math as mt
+
+x = mt.sqrt(25)
+
+print(x)
+
+x = mt.sqrt(15)
+
+print(x)
+
+print(mt.floor(2.9))
+
+print(mt.ceil(2.2))
+
+print(mt.pow(3,2))
+
+print(mt.pi)
+
+print(mt.e)
+
+print(mt.sqrt(25))
+
+from math import sqrt, pow
+
+print(pow(4,5))
