82e5a9df3e139700326563fa20fc0d6745182d7a
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-04-01 18:00:38 +0200

    Add files via upload

---
+++ prog2_d15.py
@@ -0,0 +1,16 @@
+x = 1
+
+if x == 1:
+    print('The number is ', x)
+
+elif (x == 2):
+    print('The number is', x)
+
+elif (x == 3):
+    print('The number is', x)
+
+elif (x == 4):
+    print('The number is', x)
+
+else:
+    print('it is the wrong number',x)
