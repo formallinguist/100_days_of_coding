cbcee3cf77da29fc6680bd0fb3f865c1b8f95c4f
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-04-03 00:05:05 +0200

    Add files via upload

---
+++ day_16_prog_1.py
@@ -0,0 +1,36 @@
+#For loop
+
+x = ['navin',65,2.5] # x is a list
+
+for i in x:
+    print(i)
+
+y = 'apple' #y is a string.
+
+for i in y:
+    print(i)
+
+z = [2,6,'paul']
+
+for i in z:
+    print(i)
+
+
+#using range function.
+
+for i in range(10):
+    print(i)
+
+#using range function in reverse order.
+
+for i in range(20,11,-1):
+    print(i)
+
+# using if in a for loop
+
+for i in range (20,11,-1):
+    if i % 5 != 0:
+        print(i)
+
+
+
