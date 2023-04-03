843bde952ea923e8b25699803534c56924d4f0d8
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-04-04 00:22:32 +0200

    Add files via upload

---
+++ prog1_d17.py
@@ -0,0 +1,18 @@
+for j in range(4):
+    for i in range(4):
+        print('#',end='')
+    print()   
+    
+# Triangle     
+
+for j in range(4):
+    for i in range(j+1):
+        print("#",end='')
+    print()
+    
+#Reverse of the triangle.
+
+for j in range(4):
+    for i in range (4-j):
+        print("#",end="")
+    print()
