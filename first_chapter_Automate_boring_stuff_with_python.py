e803edb5e440805928bae145bafcd982d8e9bf3d
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-07-26 19:05:59 +0530

    Add files via upload

---
+++ first_chapter_Automate_boring_stuff_with_python.py
@@ -0,0 +1,107 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2 + 2
+4
+2
+2
+2 + 3 * 6
+20
+(2 + 3) * 6
+30
+48565878 * 578453
+28093077826734
+2 ** 8
+256
+23 / 7
+3.2857142857142856
+23 // 7
+3
+23 % 7
+2
+2   +  2
+4
+(5-1)*((7+1)/(3-1))
+16.0
+#apple
+#The integer floating point and string data type.
+'Alice' + 'bob'
+'Alicebob'
+'Alice' * 5
+'AliceAliceAliceAliceAlice'
+spam = 40
+spam
+40
+eggs =  2
+spam + eggs
+42
+spam + eggs + spam
+82
+spam = spam + 2'
+SyntaxError: incomplete input
+spam = spam + 2
+spam
+42
+#Over writing the variable.
+spam = 'Hello'
+spam'
+SyntaxError: incomplete input
+spam
+'Hello'
+spam = 'Goodbye'
+spam
+'Goodbye'
+
+===================================================== RESTART: C:/Users/RAVI KIRAN/Desktop/hello.py =====================================================
+Hello world!
+What is your name?
+Ravi
+It is good to meet you,Ravi
+The length of your name is
+4
+What is your age?
+28
+You will be 29in a year.
+len('hello')
+5
+len('I flew from basque country to India')
+35
+>>> len('')
+0
+>>> str(o)
+Traceback (most recent call last):
+  File "<pyshell#32>", line 1, in <module>
+    str(o)
+NameError: name 'o' is not defined
+>>> str('0')
+'0'
+>>> str(0)
+'0'
+>>> str(-3.14)
+'-3.14'
+>>> int('42')
+42
+>>> int('-99')
+-99
+>>> int(1.25)
+1
+>>> int(1.99)
+1
+>>> float(3.14)
+3.14
+>>> float(10)
+10.0
+>>> spam = input()
+
+>>> spam = input()
+101
+>>> spam
+'101'
+>>> spam = int(spam)
+>>> spam
+101
+>>> spam * 10/5
+202.0
+>>> int(7.7)
+7
+>>> int(7.7)+1
+8
